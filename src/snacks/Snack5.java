6b3bd556dad38537180928d052508ce0883af8ad
Luca <lucacassano96@gmail.com>
2023-10-19 17:16:28 +0200

    Snack6

---
+++ src/snacks/Snack5.java
@@ -0,0 +1,16 @@
+package snacks;
+
+/**Dato un numero sotto forma di stringa, convertirlo in intero senza utilizzare funzioni già pronte.
+ Possibile usare solo :
+ cicli
+ chartAt
+ if / switch
+ Es. “25" come stringa deve essere convertito in intero 25. */
+public class Snack5 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/snacks/Snack6.java
@@ -0,0 +1,18 @@
+package snacks;
+
+//Scrivere un programma che dati dei secondi li converta in ore, minuti, secondi e
+// mostri a video la stringa generata (secondi → “hh:mm:ss”)
+public class Snack6 {
+
+    public static void main(String[] args) {
+
+        int secondi = 5432;
+        int ore = secondi / 3600;
+        int minuti = (secondi % 3600) / 60;
+        secondi = secondi % 60;
+
+        String orario = String.format("%02d:%02d:%02d", ore, minuti, secondi);
+        System.out.println(orario);
+
+    }
+}
